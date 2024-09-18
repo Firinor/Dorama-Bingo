4a5378e2f93794a4a8bec3719bda0371bbf66717
Ilya Losev <gameone.exe@gmail.com>
2024-09-18 21:41:05 +0500

    Fix BuildLoader class

--- Assets/Editor/BuildLoader.cs
+++ Assets/Editor/BuildLoader.cs
@@ -6,12 +6,8 @@ public class BuildLoader
     [MenuItem("BuildLoader/Load Data")]
     private static async void DownloadFiles()
     {
-        Debug.Log("Something");
         await EthernetManager.DoramaRemoteDownload(false);
         await EthernetManager.LanguageRemoteDownload(false);
         await EthernetManager.PostersRemoteDownload(false);
-        //StartCoroutine(EthernetManager.DoramaRemoteDownload(false));
-        //StartCoroutine(EthernetManager.LanguageRemoteDownload(false));
-        //StartCoroutine(EthernetManager.PostersRemoteDownload(false));
     }
 }
